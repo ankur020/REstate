c39d09c7921bf0c42303fd3147065519739ab7dd
Ankur <annkur020@gmail.com>
2024-04-30 20:38:20 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About RealEstate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa,
+        cupiditate nulla non consectetur sed illum, praesentium sint blanditiis
+        placeat eum, nostrum fuga esse. Facere dolore consectetur veritatis, ea
+        libero saepe repudiandae amet aliquam qui, mollitia tenetur fugiat porro
+        beatae fuga voluptas sint quis debitis? Deleniti odio ullam atque ut
+        enim.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, qui
+        id itaque consequatur iste voluptatum hic optio ad ipsum, rerum magni
+        molestias maiores totam officiis laudantium eaque? Distinctio, tenetur
+        laudantium necessitatibus alias repellendus non porro vitae ratione
+        dolore eligendi accusamus, numquam aspernatur inventore! Laudantium
+        rerum explicabo minima, ducimus commodi nostrum!
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
+        consequatur obcaecati nisi nam accusamus. Veritatis tenetur veniam ea
+        consectetur sint inventore eveniet perferendis quis, tempore, animi enim
+        repudiandae nesciunt adipisci illum quidem nihil fugiat beatae fuga
+        voluptatem doloremque quae ducimus. Esse voluptate officiis qui delectus
+        quaerat suscipit non dolorum saepe.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
